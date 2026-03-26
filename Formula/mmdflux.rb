21170375b22e8b5f68896c01ecc8f3233c4f1dd0
Kevin Swiber <kswiber@gmail.com>
2026-03-26 13:46:32 -0700

    Update mmdflux to v2.0.2
    Also fixes download URLs to use the correct mmdflux-v{version} tag format.

--- Formula/mmdflux.rb
+++ Formula/mmdflux.rb
@@ -1,21 +1,21 @@
 class Mmdflux < Formula
   desc "Render Mermaid diagrams as Unicode text, ASCII, SVG, and MMDS JSON"
   homepage "https://github.com/kevinswiber/mmdflux"
-  version "2.0.0"
+  version "2.0.2"
   license "MIT"
 
   if OS.mac?
     if Hardware::CPU.arm?
-      url "https://github.com/kevinswiber/mmdflux/releases/download/v2.0.0/mmdflux-v2.0.0-darwin-arm64.tar.gz"
-      sha256 "23a6ef87a23e6cbd45e90424e5f4e11e4ea81393583c78222071c3e354687707"
+      url "https://github.com/kevinswiber/mmdflux/releases/download/mmdflux-v2.0.2/mmdflux-v2.0.2-darwin-arm64.tar.gz"
+      sha256 "77026b80817e49954ea0fa1728f5ea57d3659c7d04c6ba2fbffe26abd96dd2b9"
     else
-      url "https://github.com/kevinswiber/mmdflux/releases/download/v2.0.0/mmdflux-v2.0.0-darwin-x86_64.tar.gz"
-      sha256 "b0348683c71e7d6441acdfa0ed78ad04366a573a8e660f84c47af2581c566ce9"
+      url "https://github.com/kevinswiber/mmdflux/releases/download/mmdflux-v2.0.2/mmdflux-v2.0.2-darwin-x86_64.tar.gz"
+      sha256 "f1bef876dfc4ad65c09af07bfe2797e0b7a93f363566e3f7b05a858ce7814b9d"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/kevinswiber/mmdflux/releases/download/v2.0.0/mmdflux-v2.0.0-linux-x86_64.tar.gz"
-      sha256 "ab7f0f78af2b1278fa16a6c15acd3d4f157cb9d706cbadd3bda3b9543b762b33"
+      url "https://github.com/kevinswiber/mmdflux/releases/download/mmdflux-v2.0.2/mmdflux-v2.0.2-linux-x86_64.tar.gz"
+      sha256 "aacb976e08f31a5e94bacd364ea60a474364d9e991603b7459ec94de04dd8ef0"
     else
       odie "Linux ARM64 binaries are not published yet."
     end
